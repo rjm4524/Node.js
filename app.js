d9ffe00a07951d3ca56cbc142ceac7a60962a4d1
rjm4524 <rjm4524@naver.com>
2024-03-07 10:47:06 +0900

    sendFile,get,post

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-console.log("안녕하세요.");
-console.log("node.js");
+const express = require('express'); //npm install express --save
+const app = express();
+
+//app.use('/폴더',express.static(__dirname+'/폴더'))
+
+app.use('/Script',express.static(__dirname+"/working.js"))
+
+app.listen(8000,()=> { 
+    console.log("누군가 들어왔습니다.")
+});
+
+//처리해주는 루틴 추가 (Spring의 Controller 역할)
+app.get('/',(req,res)=>{ //http 명령어중 get으로 요청했을때
+    console.log('첫화면')
+    res.send("어서오세요 첫화면입니다.")
+}) 
+
+app.get('/about',(req,res)=>{
+    console.log('about')
+    res.sendFile(__dirname+'/about.html')
+}) 
+
+app.get('/working',(req,res)=>{
+    console.log('working')
+    res.sendFile(__dirname+'/working.html')
+}) 
+
--- node.js
+++
@@ -1,1 +0,0 @@
-console.log("새파일");
